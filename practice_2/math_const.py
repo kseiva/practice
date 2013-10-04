cc7e927fd591886804a067b6be3a872f60f221b4
kseiva <ksyusha-ivanova@list.ru>
2013-10-05 01:11:56 +0400

    math_const.py

--- practice_2/math_const.py
+++ practice_2/math_const.py
@@ -4,7 +4,8 @@ const = {"pi": math.pi, "e": math.e, "cos": math.cos(25),
 	 "sin": math.sin(25), "exp": math.exp(2), "log": math.log10(2),
 	 "sqrt": math.sqrt(4), "tan": math.tan(4)}
 
-x = raw_input("Enter name of the constant <name>:<acc>:")
+name = raw_input("Enter the name of the constant: ")
+acc = int(input("Enter the number of decimal places: "))
 
 if name in const:
 	print 'Output', name, 'with accuracy', acc, ':', round(const[name], acc)
