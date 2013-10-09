6576d1375fb1ac43c7513b11a07036ec2445d54a
kseiva <ksyusha-ivanova@list.ru>
2013-10-09 23:06:19 +0400

    year.py

--- practice_2/year.py
+++ practice_2/year.py
@@ -1,16 +1,17 @@
-try:
-	y = int(raw_input("Please, enter year: "))
-except ValueError:
-	print('Year is entered incorrectly')
-except NameError:
-	print('Year is not set')
-else:
-	if (y % 4) == 0:
-		print('Leap year')
-	elif (y % 100) == 0:
-		print('Not a leap year')
-	elif (y % 400) == 0:
-		print('Leap year')
+def year():
+	try:
+		y = int(raw_input("Please, enter year: "))
+	except ValueError:
+		print('Year is entered incorrectly')
+	except NameError:
+		print('Year is not set')
 	else:
-		print('Not a leap year')
-
+		if (y % 4) == 0:
+			print('Leap year')
+		elif (y % 100) == 0:
+			print('Not a leap year')
+		elif (y % 400) == 0:
+			print('Leap year')
+		else:
+			print('Not a leap year')
+year()
