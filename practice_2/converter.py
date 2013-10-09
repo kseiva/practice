b0108a4a3e1b12691cb688f3549fd162703a64bb
kseiva <ksyusha-ivanova@list.ru>
2013-10-09 23:00:14 +0400

    converter.py

--- practice_2/converter.py
+++ practice_2/converter.py
@@ -1,13 +1,15 @@
-month = raw_input('Please, enter month name: ')
+def mon():
+	month = raw_input('Please, enter month name: ')
 
-name_number = {"january": 1,"february": 2,
-	       "march": 3, "april": 4,
-	       "may": 5, "june": 6,
-	       "july": 7, "august": 8,
-	       "september": 9, "october": 10, 
-	       "november": 11, "december": 12}
+	name_number = {"january": 1,"february": 2,
+	       	       "march": 3, "april": 4,
+	               "may": 5, "june": 6,
+	               "july": 7, "august": 8,
+	               "september": 9, "october": 10, 
+	               "november": 11, "december": 12}
 
-if month in name_number:
-	print 'Number of month', month, ':', name_number[month]
-else:
-	print 'Incorrect month name, try again.'
+	if month in name_number:
+		print 'Number of month', month, ':', name_number[month]
+	else:
+		print 'Incorrect month name, try again.'
+mon()
