578ed5f8c8e97dccae0a81b4160321b4a863c657
kseiva <ksyusha-ivanova@list.ru>
2013-10-09 22:41:55 +0400

    converter.py

--- practice_2/converter.py
+++ practice_2/converter.py
@@ -1,17 +1,11 @@
 month = raw_input('Please, enter month name: ')
 
-name_number = {"January": 1, "january": 1,
-	       "February": 2, "february": 2,
-	       "March": 3, "march": 3,
-	       "April": 4, "april": 4,
-	       "May": 5, "may": 5,
-	       "June": 6, "june": 6,
-	       "July": 7, "july": 7,
-	       "August": 8, "august": 8,
-	       "September": 9, "september": 9,
-	       "October": 10, "october": 10, 
-	       "November": 11, "november": 11,
-	       "December": 12, "december": 12}
+name_number = {"january": 1,"february": 2,
+	       "march": 3, "april": 4,
+	       "may": 5, "june": 6,
+	       "july": 7, "august": 8,
+	       "september": 9, "october": 10, 
+	       "november": 11, "december": 12}
 
 if month in name_number:
 	print 'Number of month', month, ':', name_number[month]
