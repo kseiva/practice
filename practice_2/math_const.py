1434382b57702bb49f30f9d2b37b305c042c1c76
kseiva <ksyusha-ivanova@list.ru>
2013-10-11 00:11:59 +0400

    math_const.py

--- practice_2/math_const.py
+++ practice_2/math_const.py
@@ -1,14 +1,13 @@
 import math
+def constant():
+	const = {"pi": math.pi, "e": math.e, "cos": math.cos(25),
+	         "sin": math.sin(25), "exp": math.exp(2), "log": math.log10(2),
+	         "sqrt": math.sqrt(4), "tan": math.tan(4)}
 
-const = {"pi": math.pi, "e": math.e, "cos": math.cos(25),
-	 "sin": math.sin(25), "exp": math.exp(2), "log": math.log10(2),
-	 "sqrt": math.sqrt(4), "tan": math.tan(4)}
+	con = raw_input("Enter <const>:<acc>: ")
+	x = con.split(":")
+	name = x[0]
+	acc = int(x[1])
+	print name, "=", round(const[name], acc)
 
-name = raw_input("Enter the name of the constant: ")
-acc = int(input("Enter the number of decimal places: "))
-
-if name in const:
-	print 'Output', name, 'with accuracy', acc, ':', round(const[name], acc)
-else:
-	print 'Incorrect constant name'
-
+constant()
