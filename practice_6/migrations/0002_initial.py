9fedc6eed977f55048698499f42192bfa50dde62
kseiva <ksyusha-ivanova@list.ru>
2013-11-08 15:50:59 +0300

    Подключение South к модели library

---
+++ practice_6/migrations/0002_initial.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Author'
+        db.create_table(u'library_author', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('first_name', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('last_name', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75, null=True)),
+        ))
+        db.send_create_signal(u'library', ['Author'])
+
+        # Adding model 'Publisher'
+        db.create_table(u'library_publisher', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=32)),
+            ('address', self.gf('django.db.models.fields.TextField')()),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('country', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('website', self.gf('django.db.models.fields.URLField')(max_length=200)),
+        ))
+        db.send_create_signal(u'library', ['Publisher'])
+
+        # Adding model 'Book'
+        db.create_table(u'library_book', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('publisher', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['library.Publisher'])),
+            ('publication_date', self.gf('django.db.models.fields.DateField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'library', ['Book'])
+
+        # Adding M2M table for field authors on 'Book'
+        m2m_table_name = db.shorten_name(u'library_book_authors')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('book', models.ForeignKey(orm[u'library.book'], null=False)),
+            ('author', models.ForeignKey(orm[u'library.author'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['book_id', 'author_id'])
+
+        # Adding model 'BooksImage'
+        db.create_table(u'library_booksimage', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('small_img', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('large_img', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('img_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('link_book', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['library.Book'])),
+            ('cont_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+        ))
+        db.send_create_signal(u'library', ['BooksImage'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Author'
+        db.delete_table(u'library_author')
+
+        # Deleting model 'Publisher'
+        db.delete_table(u'library_publisher')
+
+        # Deleting model 'Book'
+        db.delete_table(u'library_book')
+
+        # Removing M2M table for field authors on 'Book'
+        db.delete_table(db.shorten_name(u'library_book_authors'))
+
+        # Deleting model 'BooksImage'
+        db.delete_table(u'library_booksimage')
+
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'library.author': {
+            'Meta': {'object_name': 'Author'},
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        u'library.book': {
+            'Meta': {'object_name': 'Book'},
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['library.Author']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publication_date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'publisher': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['library.Publisher']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'library.booksimage': {
+            'Meta': {'object_name': 'BooksImage'},
+            'cont_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'img_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'large_img': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'link_book': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['library.Book']"}),
+            'small_img': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'library.publisher': {
+            'Meta': {'object_name': 'Publisher'},
+            'address': ('django.db.models.fields.TextField', [], {}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['library']
