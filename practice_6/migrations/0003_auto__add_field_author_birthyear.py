26d93ab203a591836f178ca63c7605649efff8d0
kseiva <ksyusha-ivanova@list.ru>
2013-11-08 16:05:48 +0300

    Модификация модели Author

---
+++ practice_6/migrations/0003_auto__add_field_author_birthyear.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Author.birthyear'
+        db.add_column(u'library_author', 'birthyear',
+                      self.gf('django.db.models.fields.IntegerField')(null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Author.birthyear'
+        db.delete_column(u'library_author', 'birthyear')
+
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'library.author': {
+            'Meta': {'object_name': 'Author'},
+            'birthyear': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'null': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        u'library.book': {
+            'Meta': {'object_name': 'Book'},
+            'authors': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['library.Author']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'publication_date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'publisher': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['library.Publisher']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'library.booksimage': {
+            'Meta': {'object_name': 'BooksImage'},
+            'cont_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'img_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'large_img': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'link_book': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['library.Book']"}),
+            'small_img': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
+        },
+        u'library.publisher': {
+            'Meta': {'object_name': 'Publisher'},
+            'address': ('django.db.models.fields.TextField', [], {}),
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'country': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'website': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['library']
---
+++ practice_6/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+import datetime
+from django.core.urlresolvers import reverse
+from control_panel.settings import MEDIA_ROOT
+
+# Create your models here.
+# -*- coding: utf-8 -*-
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField(null=True)
+    birthyear = models.IntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+    def get_absolute_url(self):
+        return reverse('control_panel.library.views.authors_list', args=[str(self.id)])
+
+
+class Publisher(models.Model):
+    title = models.CharField(max_length=32)
+    address = models.TextField()
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    website = models.URLField()
+
+    def __unicode__(self):
+        return u'%s (%s, %s)' % (self.title, self.city, self.country)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('control_panel.library.views.books_info', args=[str(self.id)])
+
+
+class BooksImage(models.Model):
+    small_img = models.ImageField(upload_to=MEDIA_ROOT)
+    large_img = models.ImageField(blank=True, null=True, upload_to=MEDIA_ROOT)
+    img_id = models.PositiveIntegerField()
+    link_book = models.ForeignKey(Book)
+    cont_type = models.ForeignKey(ContentType)
+    cont_obj = generic.GenericForeignKey("cont_type", "img_id")
+
+    def __unicode__(self):
+        return '%s' % self.id
+
+    def img_count(self):
+        count = 0
+        if self.small_img:
+            count += 1
+        if self.large_img:
+            count += 1
+        return '%s' % count
+
+    def view_small_img(self):
+        return u'<img src="%s"/>' % (self.small_img.url)
+    view_small_img.allow_tags = True
+
+    def view_large_img(self):
+        return u'<img src="%s"/>' % (self.large_img.name)
+    view_large_img.allow_tags = True
